982bd51cafcc5717c7e4681bebdb5aa8eefaf70e
Andrew Harvey <andrew@alantgeo.com.au>
2018-01-08 19:53:44 +1100

    swap upstream feed to GeoRSS which has more data
    closes #1

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # nsw-oeh-beachwatch-geojson
 
-The NSW Office of Environment and Heritage (OEH) [publishes a JSON feed of swimming suitability](http://www.environment.nsw.gov.au/beachmapp/Data) as part of [Beachwatch](http://www.environment.nsw.gov.au/beachmapp/), this project aims to make that feed more developer friendly.
+The NSW Office of Environment and Heritage (OEH) [publishes a bunch of GeoRSS feeds of swimming suitability](https://data.nsw.gov.au/data/dataset/beachwatch) as part of [Beachwatch](http://www.environment.nsw.gov.au/beachmapp/), this project aims to make that feed more developer friendly.
 
 You can either build this application into your own pipeline or use the hosted URL at https://www.beyondtracks.com/contrib/nsw-oeh-beachwatch.geojson (no service availability guarantees!), [preview it at geojson.io](http://geojson.io/#data=data:text/x-url,https://www.beyondtracks.com/contrib/nsw-oeh-beachwatch.geojson).
 
@@ -12,10 +12,11 @@ This pipeline has been built for [www.beyondtracks.com](https://www.beyondtracks
 
 # Features
 
- - **Access-Control-Allow-Origin Header** The upstream feed lacks the this header meaning web applications aren't able to use the feed directly. The sample crontab file allows you to reduce and host the Beachwatch data and serve it with your own HTTP server to add this header. This issue was reported to OEH on the 24th Nov 2015 with the reply that they will follow up with their digital strategy people and will advise once they have responded.
+ - **Data only** Where possible, the properties data void of any formatting.
+ - **GeoJSON** GeoJSON is more web developer friendly than GeoRSS (XML)
  - **Drop unneeded or duplicate properties**
  - **Coordinate Precision** Practically beach markers won't be more than 10m in accuracy so limiting to 4 decimal places will suffice.
- - **Unpack Overloaded fields** The upstream feed overloads properties into the `Categories` field. These key value is exploded out to make it easier to read in applications.
+ - **Access-Control-Allow-Origin Header** The upstream feeds lack this header meaning web applications aren't able to use the feed directly. The sample crontab file allows you to reduce and host the Beachwatch data and serve it with your own HTTP server to add this header. This issue was reported to OEH on the 24th Nov 2015 with the reply that they will follow up with their digital strategy people and will advise once they have responded.
 
 # Usage
 
@@ -27,7 +28,7 @@ Run the script with:
 
     ./bin/nsw-oeh-beachwatch-geojson nsw-oeh-beachwatch.geojson
 
-This will download the upstream feed, process it and save the resulting GeoJSON file at `nsw-oeh-beachwatch.geojson`.
+This will download the upstream feeds, process them and save the resulting GeoJSON file at `nsw-oeh-beachwatch.geojson`.
 
 # Warranty
 
--- index.js
+++ index.js
@@ -3,36 +3,83 @@ const turf = {
     point: require('@turf/helpers').point,
     featureCollection: require('@turf/helpers').featureCollection
 };
-
-// polyfill Object.entries
-const entries = require('object.entries');
-if (!Object.entries) {
-    entries.shim();
-}
+const async = require('async');
+const extend = require('xtend');
+const moment = require('moment');
+const _ = require('lodash');
 
 module.exports = {
     /**
-     * "Cleans" the NSW OEH Beachwatch Upstream Feed to:
+     * Convert the NSW OEH Beachwatch Upstream Feed to:
      *
-     *   - Drop unneded or duplicate properties
+     *   - Drop unneeded or duplicate properties
      *   - Limit coordinate precision
-     *   - Unpack overloaded Categories field
+     *   - Convert to GeoJSON
      *
-     * @param {Object} json NSW OEH Beachwatch Upstream Feed as a JSON Object
-     * @return {Object}
+     * @param {Array} georss Array of NSW OEH Beachwatch Upstream Feeds as a XML Strings
+     * @param {Function} callback Callback function invoked with (err, geojson)
      */
-    clean: function(json) {
-        if (json && json.beaches) {
-            return gp.parse(turf.featureCollection(Object.entries(json.beaches).map(([key, beach]) => {
-                var properties = {
-                    name: beach.Name,
-                    time: beach.Timestamp
-                };
-                if (beach.Categories && beach.Categories.length) {
-                    properties.status = beach.Categories[0].replace(/\/n/, '').trim().replace(/Pollution is /, '');
-                }
-                return turf.point([Number(beach.Longitude), Number(beach.Latitude)], properties);
-            })), 4);
-        }
+    convert: function(georssList, callback) {
+        return async.parallel(georssList.map(function (georss) {
+            return function (cb) {
+                // parse the upstream XML
+                require('xml2js').parseString(georss, function (err, xml) {
+                    if (err) {
+                        throw 'Error parsing XML response body';
+                    } else {
+                        cb(err, xml);
+                    }
+                });
+            };
+        }),
+        function (err, results) {
+            if (err) {
+                callback(err);
+            } else {
+                var features = _.flattenDepth(results.map(geojsonFeatures), 2);
+                callback(null, gp.parse(turf.featureCollection(features), 4));
+            }
+        });
     }
 };
+
+function geojsonFeatures(georss) {
+    if (georss && georss.rss) {
+        var channelProperties = {};
+        if (georss.rss.channel && georss.rss.channel.length && georss.rss.channel[0].item && georss.rss.channel[0].item.length) {
+            var bwData = georss.rss.channel[0]['bw:data'][0];
+
+            channelProperties.rainfall = bwData['bw:rainfall'][0];
+            channelProperties.oceanTemp = Number(bwData['bw:oceanTemp'][0]);
+            channelProperties.swell = bwData['bw:swell'][0];
+            channelProperties.highTide = bwData['bw:highTide'][0]; // TODO .match(/([^ ]+) meters at ([^.]+)./);
+            channelProperties.lowTide = bwData['bw:lowTide'][0];
+
+            return georss.rss.channel[0].item.map(function (item) {
+                var coordinates = item['georss:point'][0].split(/\s/).map(function (coord) { return Number(coord); }).reverse();
+
+                var itemProperties = {};
+
+                if ('pubDate' in item)
+                    itemProperties.pubDate = item.pubDate[0];
+
+                if ('title' in item)
+                    itemProperties.title = item.title[0];
+
+                if ('bw:data' in item && item['bw:data'].length) {
+                    var bwData = item['bw:data'][0];
+                    itemProperties.advice = bwData['bw:advice'][0].replace(/\\r/, '').replace(/\\n/, '').trim().replace(/Pollution is /, '').split('.')[0];
+                    itemProperties.patrolInfo = bwData['bw:patrolInfo'][0];
+                    itemProperties.bsg = bwData['bw:bsg'][0];
+                    itemProperties.bsgComment = bwData['bw:bsgComment'][0];
+                    itemProperties.starRating = bwData['bw:starRating'][0].length;
+                    itemProperties.dateSample = moment(bwData['bw:dateSample'][0], 'DD/MM/YYYY').format();
+                }
+
+                var properties = extend(channelProperties, itemProperties);
+
+                return turf.point(coordinates, properties);
+            });
+        }
+    }
+}
